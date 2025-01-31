b02bedfa51c2dd73affc49521e0bae6748a5a2f5
Thellman <02cathellman@gmail.com>
2025-01-31 14:16:05 -0500

    Added Libraries and While for Ellipses - Stable

--- BlockWidow.py
+++ BlockWidow.py
@@ -1,3 +1,23 @@
+#Libraries HERE
+import sys
+from time import sleep
+
+
 print('Welcome Branch - Developer: Thellman')
 
 print("\n\tWelcome to InfoTechCenter v1.0")
+
+
+x = 0
+ellipsis = 0
+
+while x != 20:
+   x += 1
+   message = ("Infotech Center System Booting" + "." * ellipsis)
+   ellipsis += 1
+   sys.stdout.write("\r" + message)
+   sleep(.5)
+   if ellipsis == 4:
+       ellipsis = 0
+   if x == 20:
+       print("\n\nOperating System Booted Up - Retina Scanned - Access Granted")
