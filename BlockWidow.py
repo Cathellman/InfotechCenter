be737c9c5e99ce8f9ef16a2b586f2cd36abbb3fa
Thellman <02cathellman@gmail.com>
2025-02-05 14:15:20 -0500

    Added Weather Choice Function and print - Stable

--- BlockWidow.py
+++ BlockWidow.py
@@ -1,8 +1,17 @@
-# Programmer: Clayton Thellman
-# Branch: Weather
-# Date:
-# Program:
+print('\n<--************************************-->\n')
 
+print('Weather Branch - Developer: Thellman\n')
 
+#Import Libraries Here!
 
+import random
+from time import sleep
 
+# Weather Function to determine the weather
+
+def weather():
+    weatherForecastList = ['snowing', 'blizzard', 'icy', 'rainy', 'windy', 'sunny', 'death']
+    weatherCondition = random.choice(weatherForecastList)
+    return weatherCondition
+
+print(weather())
