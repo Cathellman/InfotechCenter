1dc463dde31fd47b3e8a040162fc320caf53dae9
Cathellman <108098416+Cathellman@users.noreply.github.com>
2025-02-10 13:50:21 -0500

    Created VRS - function, with extra code - and death

--- BlockWidow.py
+++ BlockWidow.py
@@ -12,6 +12,40 @@ from time import sleep
 def weather():
     weatherForecastList = ['snowing', 'blizzard', 'icy', 'rainy', 'windy', 'sunny', 'death']
     weatherCondition = random.choice(weatherForecastList)
+
+    if weatherCondition == 'death':
+        if random.randint(0,50) == 5:
+           return weatherCondition
+        else:
+            weather() 
+
     return weatherCondition
 
-print(weather())
+def vehicleResponcseSystem():
+    currentWeather = weather()
+
+    if currentWeather == 'snowing': 
+        print("\nThe National Weather Service has updated our alarm by 30 minutes because of the forecast of", currentWeather)
+
+    elif currentWeather == 'blizzard': 
+        print("\nThe National Weather Service has updated our alarm by 30 minutes because of the forecast of", currentWeather)
+
+    elif currentWeather == 'icy': 
+        print("\nThe National Weather Service has updated our alarm by 30 minutes because of the forecast of", currentWeather)
+
+    elif currentWeather == 'rainy': 
+        print("\nThe National Weather Service has updated our alarm by 30 minutes because of the forecast of", currentWeather)
+
+    elif currentWeather == 'windy': 
+        print("\nThe National Weather Service has updated our alarm by 30 minutes because of the forecast of", currentWeather)
+
+    elif currentWeather == 'sunny': 
+        print("\nThe National Weather Service has updated our alarm by 30 minutes because of the forecast of", currentWeather)
+
+    elif currentWeather == 'death': 
+        print("Your Dead.")
+
+    print("\n\n")
+vehicleResponcseSystem()
+
+
