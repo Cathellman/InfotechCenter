b639cc22017bcac0e2c05d2dff051d3fa88ff2f2
Cathellman <108098416+Cathellman@users.noreply.github.com>
2025-02-10 13:56:57 -0500

    Updated code comit - stable, new wordsings VRS

--- BlockWidow.py
+++ BlockWidow.py
@@ -14,38 +14,43 @@ def weather():
     weatherCondition = random.choice(weatherForecastList)
 
     if weatherCondition == 'death':
-        if random.randint(0,50) == 5:
+        if random.randint(0,50) == 50:
            return weatherCondition
         else:
             weather() 
-
     return weatherCondition
+
+
 
 def vehicleResponcseSystem():
     currentWeather = weather()
 
     if currentWeather == 'snowing': 
-        print("\nThe National Weather Service has updated our alarm by 30 minutes because of the forecast of", currentWeather)
+        print("\nThe National Weather Service has updated our alarm by 30 minutes because of the snow\n\n Weather Condition: ", currentWeather)
 
     elif currentWeather == 'blizzard': 
-        print("\nThe National Weather Service has updated our alarm by 30 minutes because of the forecast of", currentWeather)
+        print("\nThe National Weather Service has updated our alarm by 30 minutes because of the blizzard\n\n Weather Condition: ", currentWeather)
 
     elif currentWeather == 'icy': 
-        print("\nThe National Weather Service has updated our alarm by 30 minutes because of the forecast of", currentWeather)
+        print("\nThe National Weather Service has updated our alarm by 30 minutes because its icy\n\n Weather Condition: ", currentWeather)
 
     elif currentWeather == 'rainy': 
-        print("\nThe National Weather Service has updated our alarm by 30 minutes because of the forecast of", currentWeather)
+        print("\nThe National Weather Service has updated our alarm by 30 minutes because its rainy\n\n Weather Condition: ", currentWeather)
 
     elif currentWeather == 'windy': 
-        print("\nThe National Weather Service has updated our alarm by 30 minutes because of the forecast of", currentWeather)
+        print("\nThe National Weather Service has updated our alarm by 30 minutes because its windy\n\n Weather Condition: ", currentWeather)
 
     elif currentWeather == 'sunny': 
-        print("\nThe National Weather Service has updated our alarm by 30 minutes because of the forecast of", currentWeather)
+        print("\nThe National Weather Service has not updated anything, its a good day!\n\n Weather Condition: ", currentWeather)
 
     elif currentWeather == 'death': 
         print("Your Dead.")
 
     print("\n\n")
+
+
 vehicleResponcseSystem()
+
+
 
 
