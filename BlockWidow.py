fd35c24eaebde4b087a056d7122b3d183b4432ba
Thellman <02cathellman@gmail.com>
2025-02-11 14:00:58 -0500

    Wording Comit - stable

--- BlockWidow.py
+++ BlockWidow.py
@@ -39,37 +39,30 @@ def vehicleResponcseSystem():
         print("\nThe National Weather Service has updated our alarm by 30 minutes because of the snow\n\n Weather Condition: ", currentWeather)
         sleep(delay)
         print("\nVRS has been engaged only allowing us to drive 55MPH.")
-
     elif currentWeather == 'blizzard':
         print("\nThe National Weather Service has updated our alarm by 60 minutes because of the blizzard\n\n Weather Condition: ", currentWeather)
         sleep(delay)
         print("\nVRS has been engaged only allowing us to drive 10MPH.")
-
     elif currentWeather == 'icy':
         print("\nThe National Weather Service has updated our alarm by 30 minutes because its icy\n\n Weather Condition: ", currentWeather)
         sleep(delay)
         print("\nVRS has been engaged only allowing us to drive 30MPH.")
-
     elif currentWeather == 'rainy':
         print("\nThe National Weather Service has updated our alarm by 15 minutes because its rainy\n\n Weather Condition: ", currentWeather)
         sleep(delay)
         print("\nVRS has been engaged only allowing us to drive 55MPH.")
-
     elif currentWeather == 'windy':
         print("\nThe National Weather Service has updated our alarm by 10 minutes because its windy\n\n Weather Condition: ", currentWeather)
         sleep(delay)
         print("\nVRS has been engaged only allowing us to drive 55MPH.")
-
     elif currentWeather == 'sunny':
         print("\nThe National Weather Service has not updated anything, its a good day!\n\n Weather Condition: ", currentWeather)
         sleep(delay)
         print("\nVRS has been disengaged, drive safe")
-
     elif currentWeather == 'death':
         print("Your Dead.")
         sleep(delay)
         print("\nYouve been cooked, Dirve safe :)")
-
     else:
         print("Error In The Code, Not A Proper Choice!")
 
