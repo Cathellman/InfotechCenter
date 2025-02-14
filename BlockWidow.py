319d01177a2847192e8b71ef07e1fbe76bcc50d1
Cathellman <108098416+Cathellman@users.noreply.github.com>
2025-02-14 13:53:21 -0500

    Added Libraries & gasLevelGageFuncioton - Not testes - Guithub

--- BlockWidow.py
+++ BlockWidow.py
@@ -1,4 +1,11 @@
+import random
+from time import sleep
+
 print("\n***************************\n")
 print("Gasoline Branch - Developer: Thellman\n")
+
+def gasLevelGauge():
+  gasLevelList = ['Empty', 'Low', 'Quarter Tank', 'Half Tank', 'Three Quarter Tank', 'Full Tank']
+  return random.choice(gasLevelList)
 
 
