4685723c5007e3070eb67cfe081def5f694307e0
Thellman <02cathellman@gmail.com>
2025-02-14 14:06:31 -0500

    Added gasStation list
    Working

--- BlockWidow.py
+++ BlockWidow.py
@@ -5,7 +5,12 @@ print("\n***************************\n")
 print("Gasoline Branch - Developer: Thellman\n")
 
 def gasLevelGauge():
-  gasLevelList = ['Empty', 'Low', 'Quarter Tank', 'Half Tank', 'Three Quarter Tank', 'Full Tank']
-  return random.choice(gasLevelList)
+    gasLevelList = ['Empty', 'Low', 'Quarter Tank', 'Half Tank', 'Three Quarter Tank', 'Full Tank']
+    return random.choice(gasLevelList)
+
+def gasStations():
+    gasStations = ['Shell', 'Marathon', 'SpeedWay', 'Circle K', 'Wesco', 'Meijer', 'Buc-ees', 'Cosco']
+    return random.choice(gasStations)
 
 print(gasLevelGauge())
+print(gasStations())
