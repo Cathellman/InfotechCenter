ab7d55525e16859ddd4098f8db3f6a27c92c4e26
Cathellman <108098416+Cathellman@users.noreply.github.com>
2025-02-17 13:54:16 -0500

    Created gas lev alreat, working ojn time and gose down,
    Working

--- BlockWidow.py
+++ BlockWidow.py
@@ -1,4 +1,5 @@
 import random
+from datetime import datetime
 from time import sleep
 
 print("\n***************************\n")
@@ -8,9 +9,31 @@ def gasLevelGauge():
     gasLevelList = ['Empty', 'Low', 'Quarter Tank', 'Half Tank', 'Three Quarter Tank', 'Full Tank']
     return random.choice(gasLevelList)
 
+gaslevel = gasLevelGauge()
+current_time = datetime.now().time()
+
+startTime = datetime.now().minute
+print(startTime)
+
 def gasStations():
     gasStations = ['Shell', 'Marathon', 'SpeedWay', 'Circle K', 'Wesco', 'Meijer', 'Buc-ees', 'Cosco']
     return random.choice(gasStations)
 
-print(gasLevelGauge())
-print(gasStations())
+def gaslevelCheck():
+    milesToGasStationLow = round(random.uniform(1,25),1)
+    milesToGasStationQuTank = round(random.uniform(25.1,50),1)
+
+    if gaslevel.lower == 'empty':
+        print("Out of Gas, L bozo.\n")
+        sleep(0.5)
+        print("Calling AAA")
+    elif gaslevel == 'Low':
+        print('Low. You should get a job to get more gas, imagine\n')
+        sleep(0.5)
+        print("The closest gas station is", gasStations(), "which is" , milesToGasStationLow, 'miles away\n')
+    elif gaslevel == 'Quarter Tank':
+        print('Quarter Tank. How could you.\n')
+        sleep(0.5)
+        print("The closest gas station is", gasStations(), "which is" , milesToGasStationQuTank, 'miles away\n')
+
+gaslevelCheck()
