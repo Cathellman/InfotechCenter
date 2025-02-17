48e61701d0f5d9f571af2eb3b34e0f375685c362
Cathellman <02cathellman@gmail.com>
2025-02-17 18:36:15 -0500

    Added Clear and updtated time
    Working

--- BlockWidow.py
+++ BlockWidow.py
@@ -1,55 +1,94 @@
 import random
 from datetime import datetime
 from time import sleep
+import os
+import platform
 
 print("\n***************************\n")
 print("Gasoline Branch - Developer: Thellman\n")
 
+wait = 1
+
+def getSystem():
+    os_name = platform.system()
+    if os_name == "Windows":
+        return "Windows"
+    elif os_name == "Darwin":
+        return "macOS"
+    elif os_name == "Linux":
+        return "Linux"
+    else:
+        return "Unknown OS"
+
+def clear():
+    print('\n' * 4)
+
 def gasLevelGauge():
-    gasLevelList = ['Empty', 'Low', 'Quarter Tank', 'Half Tank', 'Three Quarter Tank', 'Full Tank']
+    global  gasLevelList
+    gasLevelList = ['empty', 'Low', 'Quarter Tank', 'Half Tank', 'Three Quarter Tank', 'Full Tank']
     return random.choice(gasLevelList)
 
-gaslevel = gasLevelGauge()
-
-startTime = datetime.now().second
-
-
 def gasStations():
-    gasStations = ['Shell', 'Marathon', 'SpeedWay', 'Circle K', 'Wesco', 'Meijer', 'Buc-ees', 'Cosco']
-    return random.choice(gasStations)
+    gasStationsList = ['Shell', 'Marathon', 'SpeedWay', 'Circle K', 'Wesco', 'Meijer', 'Buc-ees', 'Cosco']
+    return random.choice(gasStationsList)
 
 def gaslevelCheck():
     milesToGasStationLow = round(random.uniform(1,25),1)
     milesToGasStationQuTank = round(random.uniform(25.1,50),1)
 
-    if gaslevel.lower == 'empty':
-        print("Out of Gas, L bozo.\n")
-        sleep(0.5)
+    clear()
+    if gaslevel == 'empty':
+        print("Empty. \n")
+        sleep(wait)
+        clear()
         print("Calling AAA")
+
     elif gaslevel == 'Low':
-        print('Low. You should get a job to get more gas, imagine\n')
-        sleep(0.5)
+        print('Low. \n')
+        sleep(wait)
+        clear()
         print("The closest gas station is", gasStations(), "which is" , milesToGasStationLow, 'miles away\n')
+
     elif gaslevel == 'Quarter Tank':
-        print('Quarter Tank. How could you.\n')
-        sleep(0.5)
+        print('1/4. \n')
+        sleep(wait)
+        clear()
         print("The closest gas station is", gasStations(), "which is" , milesToGasStationQuTank, 'miles away\n')
+
     elif gaslevel == 'Half Tank':
-        print('Half Tank.\n')
+        print('2/4 Tank.\n')
+        sleep(wait)
+        clear()
+
     elif gaslevel == 'Three Quarter Tank':
-        print('Three Quarter Tank.\n')
+        print('3/4 Tank.\n')
+        sleep(wait)
+        clear()
+
     else:
         print('Full')
-        
+        sleep(wait)
+        clear()
 
+
+systemOS = getSystem()
+
+gaslevel = gasLevelGauge()
+
+startTime = datetime.now().second
 
 gaslevelCheck()
 i = 0
 while True:
-    if startTime + 5 < datetime.now().second:
+    sleep(0.1)
+    if startTime + 3 < datetime.now().second:
         startTime = datetime.now().second
         i = i + 1
         gaslevelCheck()
         if i > 2:
             i = 0
-            gaslevel = gasLevelGauge()
+            index = gasLevelList.index(gaslevel)
+            if index == 0:
+                print("Get gas now\n")
+            else:
+                gaslevel = gasLevelList[index - 1]
